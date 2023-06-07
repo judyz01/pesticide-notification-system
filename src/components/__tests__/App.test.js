5336d8767d1a9bce544bae0c7171f3d0ce28c306
Judy Zhang <judy.j.zhang@gmail.com>
2023-06-07 00:45:17 -0700

    cypress e2e tests

--- src/components/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer';
-import { render, screen, cleanup } from '@testing-library/react';
-
-it("App renders correctly", () => {
-  // render(<App />);
-  expect(true).toBe(true);
-});
-
